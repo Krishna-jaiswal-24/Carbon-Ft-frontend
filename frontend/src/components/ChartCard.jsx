6bc7a262e7e11c774054c19bd051d0dc12decac0
Krishna Jaiswal <krishnajaiswal2119@gmail.com>
2024-04-22 02:52:32 +0530

    Added the chart functionality

--- frontend/src/components/ChartCard.jsx
+++ frontend/src/components/ChartCard.jsx
@@ -9,42 +9,83 @@ import {
 	Tooltip
 } from 'chart.js';
 import React from 'react';
-import { Line } from 'react-chartjs-2';
-
-
+import {Line} from 'react-chartjs-2';
 
 
 ChartJS.register(LineElement, PointElement, CategoryScale, Legend, LinearScale, Title, Tooltip);
-const ChartCard = () => {
-	const options = {}
+const ChartCard = (props) => {
 
-	const data = {
-		labels: ['1', '2', '3', '4', '5', '6', '7', '1', '2', '3', '4', '5', '6', '7'],
-		datasets: [
-			{
-				label: 'CO2e',
-				data: [82, 1, 47, 9, 64, 30, 5, 21, 73, 94],
-				fill: false,
-				borderColor: "#F7DCB9"
+	console.log(props.data)
+		const options = {
+			scales: {
+				x: {
+					ticks: {
+						color: '#2e3918',
+						font:{
+							size: 10
+						}// Set the x-axis label color
+					},
+					title: {
+						display: true,
+						text: 'Time',
+						color: '#2e3918',
+						font: {
+							size: 24
+						},
+							// Set the x-axis title color
+					}
+				},
+				y: {
+					ticks: {
+						color: '#2e3918',
+					// Set the y-axis label color
+					},
+					title: {
+						display: true,
+						text: 'CO2 Emissions',
+						size: '2rem',
+						color: '#2e3918' ,
+						font:{
+							size: 24
+						}// Set the y-axis title color
+					}
+				}
 			},
-			{
-				label: 'CO2f',
-				data: [7, 23, 99, 42, 88, 15, 10, 6, 51, 3],
-				fill: false,
-				borderColor: "red",
+			plugins: {
+				legend: false,
+
 			},
-		],
+			responsive: true
+		};
 
-	};
 
+const keys = [...props.data.keys()];
+const values = [...props.data.values()];
+console.log(keys);
+console.log(values);
 
-	return (
-		<div className="h-[31rem] w-3/5 p-8 rounded-lg bg-primary ml-6">
-			<h1 className="text-secondary font-albert text-5xl">Graph</h1>
-			<div className="h-[90%]"> <Line options={options} data={data} /></div>
+const data = {
+	labels: keys,
+	datasets: [
+		{
+			label: 'CO2e',
+			data: values,
+			fill: true,
+			borderColor: '#d0fc65',
+		}
+	],
 
-		</div>
-	);
 };
+
+
+return (
+	<div className="h-[31rem] w-3/5 p-8 rounded-lg bg-primary ml-6">
+		<h1 className="text-secondary font-albert text-5xl">Graph</h1>
+		<div className="h-[90%]"><Line options={options} data={data}/></div>
+
+	</div>
+);
+}
+;
 
 export default ChartCard;
--- frontend/src/components/FootprintCard.jsx
+++ frontend/src/components/FootprintCard.jsx
@@ -8,12 +8,20 @@ const FootprintCard = (props) => {
 	const handleClick = () => {
 		navigate('/breakdown',{state:props});
 	}
-	return (
-		<div className="bg-primary w-fit flex flex-col items-start p-4 justify-center h-40 rounded-lg my-8 mx-4 cursor-pointer" >
-			<p className={" font-albert text-3xl font-bold text-secondary"} onClick={handleClick}><span className='text-5xl'>{props.totalEmission}</span> CO2e</p>
-			<h1 className="text-white font-albert">{props.timestamp}</h1>
+	let date = new Date(props.timestamp);
+	// Extracting the date parts
+	let year = date.getFullYear();
+	let month = date.getMonth() + 1; // getMonth() returns 0-11
+	let day = date.getDate();
 
-			<a href={`https://sepolia.etherscan.io/tx/${props.txHash}`} target="_blank" rel="noopener noreferrer" className='text-secondary'>Transaction 🔗</a>
+// Formatting the date as YYYY-MM-DD
+	let formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+	return (
+		<div className="bg-primary w-[20rem] flex flex-col items-start p-4 gap-x-10 justify-center h-40 rounded-lg my-8 mx-4 cursor-pointer" >
+			<p className={" font-albert text-3xl font-bold text-secondary"} onClick={handleClick}><span className='text-5xl'>{props.totalEmission}</span> CO2e</p>
+			<h1 className="text-white font-albert text-xl">{formattedDate}</h1>
+
+			<a href={`https://sepolia.etherscan.io/tx/${props.txHash}`} target="_blank" rel="noopener noreferrer" className='text-secondary text-xl'>Transaction 🔗</a>
 		</div>
 	);
 };
